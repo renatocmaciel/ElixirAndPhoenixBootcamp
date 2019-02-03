8ba61ab90381fb1f9dafe2e4bee30571aed8f59b
Renato Maciel <renato.cmaciel@hotmail.com>
2019-02-03 13:23:56 -0200

    section 9 done

--- discuss/web/models/topic.ex
+++ discuss/web/models/topic.ex
@@ -1,7 +1,13 @@
 defmodule Discuss.Topic do
-  use Disscus.Web, :model
+  use Discuss.Web, :model
 
   schema "topics" do
     field :title, :string
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
   end
 end
